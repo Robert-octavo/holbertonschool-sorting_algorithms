40274f3e59c37c58675138930c82ff1ff3b2c10e
SebastianBlandon <blandonodz@gmail.com>
2022-06-17 00:15:09 -0500

    [UPDATED] 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -22,7 +22,6 @@ void _swap(int *a, int *b)
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, minor;
-	int *array_cpy;
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -34,22 +33,11 @@ void selection_sort(int *array, size_t size)
 				minor = j;
 			}
 		}
-		for (i = 0; i < size - 1; i++)
-		{
-			array_cpy[i] = array[i];
-		}
 
 		if (minor != i)
 		{
 			_swap(&array[i], &array[minor]);
 		}
-		for (i = 0; i < size - 1; i++)
-		{
-			if (array_cpy[i] != array[i])
-			{
-				print_array(array, size);
-				break;
-			}
-		}
+		print_array(array, size);
 	}
 }
