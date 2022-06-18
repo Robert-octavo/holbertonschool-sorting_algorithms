a8b446f1d266c98d5d462612df91ec32cedea398
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-17 23:20:06 -0500

    betty correction

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,22 +1,21 @@
 #include "sort.h"
+listint_t *_swap(listint_t **current, listint_t **sorted);
 /**
  * insertion_sort_list - function that sorts a doubly linked
  * list of integers in ascending order using the Insertion
  * sort algorithm
- * 
  * @list: pointer to the head of a doubly-linked list of integers
  */
-listint_t *_swap(listint_t **current, listint_t **sorted);
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current;
+	listint_t *current;
 	listint_t *swap;
 
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-    swap = *list;
+	swap = *list;
 	current = (*list)->next;
 
 	while (current != NULL)
@@ -47,6 +46,12 @@ void insertion_sort_list(listint_t **list)
 	}
 }
 
+/**
+ * _swap - swaps the numbers in the list
+ * @current: pointer to a node in the list
+ * @sawp: pointer to a node in the list
+ * Return: pointer to a listint_t*
+ */
 listint_t *_swap(listint_t **current, listint_t **sawp)
 {
 	listint_t *tmp;
@@ -59,10 +64,10 @@ listint_t *_swap(listint_t **current, listint_t **sawp)
 		tmp->prev = *sawp;
 	}
 	if (*current != NULL)
-    {
-        (*current)->next = *sawp;
+	{
+		(*current)->next = *sawp;
 		(*current)->prev = (*sawp)->prev;
-    }
+	}
 	if ((*sawp)->prev != NULL)
 		(*sawp)->prev->next = *current;
 	((*sawp)->prev) = *current;
