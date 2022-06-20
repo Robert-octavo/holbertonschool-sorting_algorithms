ac9fa5b09e01a97cab83f69eaf484181dcb8a2e7
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-20 00:49:57 -0500

    Just the file

---
+++ 104-heap_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * heap_sort - function that sorts an array of integers in
+ * ascending order using the Heap sort algorithm
+ * You must implement the sift-down heap sort algorithm
+ * @array: array of ints to sort
+ * @size: size of the array to sort
+ */
+void heap_sort(int *array, size_t size)
+{
+	if (array == NULL || size <= 1)
+		return;
+}
